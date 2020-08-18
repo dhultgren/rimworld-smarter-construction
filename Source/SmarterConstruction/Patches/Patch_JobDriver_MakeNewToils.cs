fb92ae1a55894cd40fd4523734e954bae1b7ddee
dhultgren <hultgren.daniel@gmail.com>
2020-08-18 22:46:16 +0200

    Halfassed incomplete implementation of the same logic for install jobs

--- Source/SmarterConstruction/Patches/Patch_JobDriver_MakeNewToils.cs
+++ Source/SmarterConstruction/Patches/Patch_JobDriver_MakeNewToils.cs
@@ -2,6 +2,7 @@
 using RimWorld;
 using SmarterConstruction.Pathfinding;
 using System.Collections.Generic;
+using System.Linq;
 using Verse;
 using Verse.AI;
 
@@ -30,4 +31,32 @@ namespace SmarterConstruction.Patches
             }
         }
     }
+
+
+    // Fail during construction if the building would enclose something
+    /*[HarmonyPatch(typeof(JobDriver_HaulToContainer), "MakeNewToils")]
+    public class Patch_JobDriver_MakeNewToils2
+    {
+        public static IEnumerable<Toil> Postfix(IEnumerable<Toil> __result, JobDriver_HaulToContainer __instance, Pawn ___pawn, Job ___job)
+        {
+            var thing = __instance.ThingToCarry.GetInnerIfMinified();
+            var passability = thing?.def?.passability;
+            if (thing != null && passability == Traversability.Impassable && ___job.targetB.Thing is Blueprint)
+            {
+                //__instance.AddFailCondition(() => ClosedRegionDetector.WouldEncloseThings(___job.targetA.Thing, ___pawn));
+                var toisl = new List<Toil>(__result);
+                var lol = __result
+                    .Where(t => t.defaultCompleteMode == ToilCompleteMode.Delay)
+                    .ToList();
+                foreach(var t in toisl)
+                {
+                    if (t.defaultCompleteMode == ToilCompleteMode.Delay)
+                    {
+                        t.AddFailCondition(() => ClosedRegionDetector.WouldEncloseThings(___job.targetB.Thing, ___pawn));
+                    }
+                    yield return t;
+                }
+            }
+        }
+    }*/
 }
---
+++ Source/SmarterConstruction/Patches/Patch_WorkGiver_ConstructDeliverResources.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+using RimWorld;
+using SmarterConstruction.Pathfinding;
+using Verse;
+using Verse.AI;
+
+namespace SmarterConstruction.Patches
+{
+    // Pretend job doesn't exist if the building would enclose something
+    /*[HarmonyPatch(typeof(WorkGiver_ConstructDeliverResources), "ResourceDeliverJobFor")]
+    public class Patch_WorkGiver_ConstructDeliverResources
+    {
+        public static bool Prefix(Pawn pawn, IConstructible c, ref Job __result, WorkGiver_ConstructDeliverResources __instance)
+        {
+            return !(c is Blueprint_Install install
+                && install.ThingToInstall?.def?.passability == Traversability.Impassable
+                && ClosedRegionDetector.WouldEncloseThings(install, pawn));
+        }
+    }*/
+}
