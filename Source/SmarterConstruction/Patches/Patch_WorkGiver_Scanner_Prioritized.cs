304ce987d5f610cd12489af5d2c1a53b02ef2a28
dhultgren <hultgren.daniel@gmail.com>
2020-09-14 14:54:36 +0200

    More correct CIL way to check if object is <Type>

--- Source/SmarterConstruction/Patches/Patch_WorkGiver_Scanner_Prioritized.cs
+++ Source/SmarterConstruction/Patches/Patch_WorkGiver_Scanner_Prioritized.cs
@@ -10,17 +10,14 @@ namespace SmarterConstruction.Patches
     [HarmonyPatch("Prioritized", MethodType.Getter)]
     public class Patch_WorkGiver_Scanner_Prioritized
     {
-        public static List<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
         {
-            var retTrue = generator.DefineLabel();
             var ret = new List<CodeInstruction>
             {
                 new CodeInstruction(OpCodes.Ldarg_0),
                 new CodeInstruction(OpCodes.Isinst, typeof(WorkGiver_ConstructFinishFrames)),
-                new CodeInstruction(OpCodes.Brtrue, retTrue),
-                new CodeInstruction(OpCodes.Ldc_I4_0),
-                new CodeInstruction(OpCodes.Ret),
-                new CodeInstruction(OpCodes.Ldc_I4_1).WithLabels(retTrue),
+                new CodeInstruction(OpCodes.Ldnull),
+                new CodeInstruction(OpCodes.Cgt_Un),
                 new CodeInstruction(OpCodes.Ret)
             };
             return ret;
