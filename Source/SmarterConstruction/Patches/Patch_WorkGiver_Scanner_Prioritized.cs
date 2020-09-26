094e37af141e5bf161685ead208b26f6946b0017
dhultgren <hultgren.daniel@gmail.com>
2020-09-26 18:55:22 +0200

    Transpile different function to get around the current Harmony + Linux problem

--- Source/SmarterConstruction/Patches/Patch_WorkGiver_Scanner_Prioritized.cs
+++ Source/SmarterConstruction/Patches/Patch_WorkGiver_Scanner_Prioritized.cs
@@ -1,26 +1,33 @@
 ﻿using HarmonyLib;
 using RimWorld;
 using System.Collections.Generic;
+using System.Linq;
 using System.Reflection.Emit;
+using Verse;
 
 namespace SmarterConstruction.Patches
 {
     // Tell Rimworld that WorkGiver_ConstructFinishFrames should look at priority
-    [HarmonyPatch(typeof(WorkGiver_Scanner))]
-    [HarmonyPatch("Prioritized", MethodType.Getter)]
-    public class Patch_WorkGiver_Scanner_Prioritized
+    [HarmonyPatch(typeof(JobGiver_Work), "TryIssueJobPackage")]
+    public class PatchMakeFinishFramesPrioritized
     {
-        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        static List<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
         {
-            var ret = new List<CodeInstruction>
+            var instructionList = instructions.ToList();
+            var prioritizedCallIndex = instructionList.FirstIndexOf(instruction => instruction.opcode == OpCodes.Callvirt && instruction.ToString().Contains("WorkGiver_Scanner::get_Prioritized"));
+
+            var newInstructions = new[]
             {
-                new CodeInstruction(OpCodes.Ldarg_0),
+                instructionList[prioritizedCallIndex-2].Clone(),
+                instructionList[prioritizedCallIndex-1].Clone(),
                 new CodeInstruction(OpCodes.Isinst, typeof(WorkGiver_ConstructFinishFrames)),
                 new CodeInstruction(OpCodes.Ldnull),
                 new CodeInstruction(OpCodes.Cgt_Un),
-                new CodeInstruction(OpCodes.Ret)
+                new CodeInstruction(OpCodes.Or)
             };
-            return ret;
+
+            instructionList.InsertRange(prioritizedCallIndex + 1, newInstructions);
+            return instructionList;
         }
     }
 }
