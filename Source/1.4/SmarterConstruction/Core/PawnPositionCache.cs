aa42fc9c31ca22182a137875d2a89db03692d54e
dhultgren <hultgren.daniel@gmail.com>
2022-11-21 21:29:25 +0100

    Add 1.4 support

---
+++ Source/1.4/SmarterConstruction/Core/PawnPositionCache.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using Verse;
+
+namespace SmarterConstruction.Core
+{
+    class PawnPositionCache
+    {
+        private static readonly int PawnStuckAfter = 300;
+        private static readonly int CacheCleanupFrequency = 2000;
+
+        private static readonly Dictionary<Pawn, Tuple<IntVec3, int>> positionCache = new Dictionary<Pawn, Tuple<IntVec3, int>>();
+        private static int lastCacheCleanup = Find.TickManager.TicksGame;
+
+        public static bool IsPawnStuck(Pawn pawn)
+        {
+            if (pawn?.Position == null) return false;
+            CleanCache();
+
+            if (positionCache.ContainsKey(pawn))
+            {
+                var data = positionCache[pawn];
+                if (data.Item1 == pawn.Position)
+                {
+                    if (data.Item2 <= Find.TickManager.TicksGame)
+                    {
+                        positionCache.Remove(pawn);
+                        return true;
+                    }
+                    return false;
+                }
+            }
+            positionCache[pawn] = new Tuple<IntVec3, int>(pawn.Position, Find.TickManager.TicksGame + PawnStuckAfter);
+            return false;
+        }
+
+        private static void CleanCache()
+        {
+            if (lastCacheCleanup + CacheCleanupFrequency < Find.TickManager.TicksGame)
+            {
+                positionCache.RemoveAll(d => d.Value.Item2 + CacheCleanupFrequency < Find.TickManager.TicksGame);
+                lastCacheCleanup = Find.TickManager.TicksGame;
+            }
+        }
+    }
+}
---
+++ Source/1.4/SmarterConstruction/Patches/WorkGiver_ConstructFinishFrames_JobOnThing.cs
@@ -0,0 +1,44 @@
+﻿using HarmonyLib;
+using RimWorld;
+using SmarterConstruction.Core;
+using Verse;
+using Verse.AI;
+
+namespace SmarterConstruction.Patches
+{
+    // Pretend job doesn't exist if the building would enclose something
+    [HarmonyPatch(typeof(WorkGiver_ConstructFinishFrames), "JobOnThing")]
+    public class WorkGiver_ConstructFinishFrames_JobOnThing
+    {
+        private static readonly int ThrottleJobsAfter = 20;
+        private static int currentTick = 0;
+        private static int thingsUpdatedThisTick = 0;
+
+        public static void Postfix(Pawn pawn, Thing t, ref Job __result, bool forced, WorkGiver_ConstructFinishFrames __instance)
+        {
+            if (__result != null && t?.def?.entityDefToBuild?.passability == Traversability.Impassable && !forced)
+            {
+                if (currentTick != Find.TickManager.TicksGame)
+                {
+                    currentTick = Find.TickManager.TicksGame;
+                    thingsUpdatedThisTick = 0;
+                }
+
+                // Minimize stuttering by throttling checks if there are too many each tick
+                // Will still run the detection code if there isn't any cached data available, otherwise it could cause a job loop
+                var maxCacheLength = thingsUpdatedThisTick < ThrottleJobsAfter ? SmarterConstruction.Settings.GetJobCacheTicks : SmarterConstruction.Settings.GetJobThrottleCacheTicks;
+                var encloseData = ClosedRegionDetector.WouldEncloseThings(t, pawn, maxCacheLength);
+
+                if (encloseData.EnclosesSelf)
+                {
+                    DebugUtils.VerboseLog($"Allowing self enclosing job on {t.Position} for pawn {pawn.Label} expecting a teleport.");
+                }
+                if (encloseData.EnclosesThings)
+                {
+                    __result = null;
+                }
+                thingsUpdatedThisTick++;
+            }
+        }
+    }
+}
