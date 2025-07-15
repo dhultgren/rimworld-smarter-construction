0dce2efcc44f3ca2dda95d843d36a67d3976ced9
dhultgren <hultgren.daniel@gmail.com>
2025-07-15 13:46:49 +0200

    Skip ReadyForNextToil() to avoid cleaning up multiple times

--- Source/1.6/SmarterConstruction/Patches/Patch_JobDriver_MakeNewToils.cs
+++ Source/1.6/SmarterConstruction/Patches/Patch_JobDriver_MakeNewToils.cs
@@ -28,31 +28,25 @@ namespace SmarterConstruction.Patches
         {
             var completeConstructionCall = typeof(Frame).GetMethod("CompleteConstruction");
             var testEncloseCall = typeof(TranspilerHelper).GetMethod(nameof(TranspilerHelper.EndJobIfEnclosing));
-            var readyForNextToilCall = typeof(JobDriver).GetMethod(nameof(JobDriver.ReadyForNextToil));
             var workDoneField = typeof(Frame).GetField(nameof(Frame.workDone));
             var instructionList = instructions.ToList();
 
             var completeConstructionIndex = instructionList.FindIndex(instruction =>
                 instruction.opcode == OpCodes.Callvirt && instruction.Calls(completeConstructionCall));
             var insertionIndex = completeConstructionIndex - 2;
-            var readyForNextToilInstruction = instructionList
+            var retInstruction = instructionList
                 .Skip(insertionIndex)
-                .First(instruction =>
-                    (instruction.opcode == OpCodes.Call || instruction.opcode == OpCodes.Callvirt) &&
-                    instruction.operand is MethodInfo mi &&
-                    mi.Name == "ReadyForNextToil");
-            var readyForNextToilLabel = generator.DefineLabel();
-            readyForNextToilInstruction.labels.Add(readyForNextToilLabel);
+                .First(instruction => instruction.opcode == OpCodes.Ret && instruction.labels.Count > 0);
 
             instructionList.InsertRange(insertionIndex, new[] {
                 new CodeInstruction(OpCodes.Ldloc_1),                               // Set workDone = workToBuild to avoid graphic glitch
                 new CodeInstruction(OpCodes.Ldloc_3),                               //
                 new CodeInstruction(OpCodes.Stfld, workDoneField),                  //
 
-                new CodeInstruction(OpCodes.Ldloc_1),                               // If EndJobIfEnclosing returns true, skip to ReadyForNextToil
-                new CodeInstruction(OpCodes.Ldloc_0),                               //
+                new CodeInstruction(OpCodes.Ldloc_1),                               // If EndJobIfEnclosing returns true, skip CompleteConstruction() and ReadyForNextToil()
+                new CodeInstruction(OpCodes.Ldloc_0),                               // Calling both EndCurrentJob and ReadyForNextToil can result in cleaning up the job multiple times
                 new CodeInstruction(OpCodes.Call, testEncloseCall),                 //
-                new CodeInstruction(OpCodes.Brtrue, readyForNextToilLabel)          //
+                new CodeInstruction(OpCodes.Brtrue, retInstruction.labels[0])       //
             });
 
             return instructionList;
@@ -69,10 +63,10 @@ namespace SmarterConstruction.Patches
             var wouldEnclose = ClosedRegionDetector.WouldEncloseThings(target, pawn, SmarterConstruction.Settings.FinishCacheTicks);
             if (wouldEnclose.EnclosesThings)
             {
+                DebugUtils.DebugLog($"Ending job for {pawn.Label} at {target.Position} because it would enclose something.");
                 pawn.jobs.EndCurrentJob(JobCondition.Incompletable);
                 return true;
             }
-            DebugUtils.VerboseLog(pawn.Label + " finished " + target.Label + " on coordinates " + target.Position);
 
             var pawnsAtLocation = target.Position.GetThingList(target.Map)
                 .Where(t => t is Pawn && t.Faction != null && t.Faction.IsPlayer)
@@ -85,6 +79,7 @@ namespace SmarterConstruction.Patches
                 if (safePositions.Count == 0)
                 {
                     pawn.jobs.EndCurrentJob(JobCondition.Incompletable);
+                    DebugUtils.DebugLog($"No safe positions found for {pawn.Label} at {target.Position}, ending job.");
                     return true;
                 }
                 else
@@ -98,6 +93,7 @@ namespace SmarterConstruction.Patches
                 }
             }
 
+            DebugUtils.VerboseLog($"{pawn.Label} finished {target.Label} at {target.Position}");
             Patch_WorkGiver_Scanner_GetPriority.RemoveNeighborCachedData(target);
             return false;
         }
